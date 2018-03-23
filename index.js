be190d882d6d25d2a64371574674a94007a5fce7
VANYA-PC\VANYA <rst629@mail.ru>
2018-03-23 15:16:35 +0700

    'express'

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ const nodemailer = require('nodemailer');
 const fs = require('fs')
 const TOKEN ='485746843:AAGCaSU18nlCA8etTC23Vj-OaA_a4s9BJrc'
 
-const bot = new TelegramBot(TOKEN)
+const bot = new TelegramBot(TOKEN, {
+    polling:true
+})
 
+const express = require('express');
+const app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 
 
